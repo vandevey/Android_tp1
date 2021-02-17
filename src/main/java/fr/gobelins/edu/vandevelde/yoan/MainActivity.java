0c699f045cb8b6e77998998857561ce6a6f062dd
Yoan Vandevelde <yoan.vandevelde.isn@gmail.com>
2021-02-17 15:51:58 +0100

    init

---
+++ src/main/java/fr/gobelins/edu/vandevelde/yoan/MainActivity.java
@@ -0,0 +1,51 @@
+package fr.gobelins.edu.vandevelde.yoan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonToast;
+    private Button buttonInc;
+    private TextView textViewCount;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    /**
+     * Methods that allow to retrieve the instance of views in the layout
+     */
+    private void initViews() {
+        buttonToast = findViewById(R.id.toast);
+        buttonInc = findViewById(R.id.increment);
+        textViewCount = findViewById(R.id.textCounter);
+
+        // Intercept click on the compute button
+        buttonInc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //incrémenter la valeur du compteur puis mettre à jour le text
+                counter++;
+                textViewCount.setText(""+counter);
+
+                // Pour afficher un toast
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+
+}
