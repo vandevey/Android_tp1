4ac03af7f5fe48ce63a406152c0da1284c1e6b24
vandevey <yoan.vandevelde.isn@gmail.com>
2021-02-18 11:47:42 +0100

    home navigation + send mail

--- src/main/java/fr/gobelins/edu/vandevelde/yoan/HomeActivity.java
+++ src/main/java/fr/gobelins/edu/vandevelde/yoan/HomeActivity.java
@@ -1,4 +1,48 @@
 package fr.gobelins.edu.vandevelde.yoan;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.gobelins.edu.vandevelde.yoan.databinding.ActivityHomeBinding;
+import fr.gobelins.edu.vandevelde.yoan.databinding.ActivitySecondBinding;
+
+import static java.lang.Integer.parseInt;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.calculateButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+            startActivity(intent);
+        });
+        binding.toastButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+        });
+        binding.mailButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            String[] addresses = {"yoan.vandevelde@edu.gobelins.fr"};
+            intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Sujet");
+            intent.putExtra(Intent.EXTRA_TEXT, "Salut");
+
+            startActivity(Intent.createChooser(intent, "Send Email"));
+        });
+
+
+    }
+
 }
